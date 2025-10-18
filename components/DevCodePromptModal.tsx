56728d188c085d5676eb740eba38735bab4c2ad4
SAT18 official <sayyidagustian@gmail.com>
2025-10-18 07:13:22 +0700

    Change DEV_SECRET_CODE to an empty string
    Signed-off-by: SAT18 official <sayyidagustian@gmail.com>

--- components/DevCodePromptModal.tsx
+++ components/DevCodePromptModal.tsx
@@ -6,7 +6,7 @@ interface DevCodePromptModalProps {
     onClose: () => void;
 }
 
-const DEV_SECRET_CODE = '18';
+const DEV_SECRET_CODE = '';
 
 const DevCodePromptModal: React.FC<DevCodePromptModalProps> = ({ onSuccess, onClose }) => {
     const [code, setCode] = useState('');
